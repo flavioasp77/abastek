a95c3faed5eb41759d1965d6fc54695d6e73de21
flavioasp77 <flavioasp77@gmail.com>
2025-03-29 15:10:48 -0300

    criando os services

---
+++ backend/src/main/java/com/abastek/backend/service/MaintenanceService.java
@@ -0,0 +1,58 @@
+package com.abastek.backend.service;
+
+import com.abastek.backend.model.Equipment;
+import com.abastek.backend.model.Maintenance;
+import com.abastek.backend.repository.MaintenanceRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MaintenanceService {
+    private final MaintenanceRepository maintenanceRepository;
+    private final EquipmentService equipmentService;
+
+    public MaintenanceService(MaintenanceRepository maintenanceRepository, EquipmentService equipmentService) {
+        this.maintenanceRepository = maintenanceRepository;
+        this.equipmentService = equipmentService;
+    }
+
+    public List<Maintenance> getAllMaintenances() {
+        return maintenanceRepository.findAll();
+    }
+
+    public Optional<Maintenance> getMaintenanceById(Long id) {
+        return maintenanceRepository.findById(id);
+    }
+
+    public Maintenance  createMaintenance(Maintenance maintenance) {
+        Equipment equipment = maintenance.getEquipment();
+
+        if (equipment != null) {
+            Optional<Equipment> existingEquipment = equipmentService.getEquipmentById(equipment.getId());
+            if (existingEquipment.isPresent()) {
+                Equipment existing = existingEquipment.get();
+                if (maintenance.getMaintenanceDate().isBefore(existing.getManufactureDate())) {
+                    throw new IllegalArgumentException("Maintenance date cannot be before the manufacture date of the equipment.");
+                }
+            }
+        }
+        return maintenanceRepository.save(maintenance);
+    }
+
+    public Maintenance updateMaintenance(Long id, Maintenance updatedMaintenance) {
+        Optional<Maintenance> existingMaintenance = maintenanceRepository.findById(id);
+        if (existingMaintenance.isPresent()) {
+            Maintenance maintenance = existingMaintenance.get();
+            maintenance.setDescription(updatedMaintenance.getDescription());
+            maintenance.setMaintenanceDate(updatedMaintenance.getMaintenanceDate());
+            return maintenanceRepository.save(maintenance);
+        } else {
+            return null;
+        }
+    }
+
+    public void deleteMaintenance(Long id) {
+        maintenanceRepository.deleteById(id);
+    }
+}
